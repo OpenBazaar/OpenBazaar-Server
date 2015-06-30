f835e98be5213c3f39922f5d2b0e38b91a561d81
Chris <ctpacia@gmail.com>
2015-06-30 14:48:19 -0400

    Added python guid module
    Turns out since the pynacl python library uses C, using python isn't that much slower
    than just using straight C to create the guid. It's about 15% slower.
    
    I've included both python and C guid modules and we can decide which one we
    want to use later.

---
+++ guidc/guid.py
@@ -0,0 +1,16 @@
+__author__ = 'chris'
+import nacl.signing
+import nacl.hash
+
+def generate():
+    def testpow(pow):
+        return True if int(pow, 16) < 50 else False
+    valid_pow = False
+    while not valid_pow:
+        signing_key = nacl.signing.SigningKey.generate()
+        verify_key = signing_key.verify_key
+        signed = signing_key.sign(str(verify_key))
+        h = nacl.hash.sha512(signed)
+        pow = h[64:128]
+        valid_pow = testpow(pow[:6])
+    return signing_key.encode()
--- guidc/setup.py
+++ guidc/setup.py
@@ -1,5 +1,5 @@
 __author__ = 'chris'
 from distutils.core import setup, Extension
 
-setup(name='guidc', version='0.0',
-	ext_modules = [Extension('guidc', sources=['guidc.c'], libraries=['sodium'])])
+setup(name='guid', version='0.0',
+	ext_modules = [Extension('guid', sources=['guid.c'], libraries=['sodium'])])
