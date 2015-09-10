3a70707e5ae6d6527e786dd35286f9bce6135839
Chris <ctpacia@gmail.com>
2015-09-10 13:04:42 -0400

    Run as daemon on unix TODO: write code to run as windows service

--- openbazaard.py
+++ openbazaard.py
@@ -1,10 +1,13 @@
 __author__ = 'chris'
 import os
 import sys
+import argparse
+import platform
 from twisted.internet import reactor
 from twisted.python import log, logfile
 from twisted.web.server import Site
 from twisted.web.static import File
+from daemon import Daemon
 
 import stun
 from autobahn.twisted.websocket import listenWS
@@ -23,79 +26,155 @@ from api.ws import WSFactory, WSProtocol
 from api.restapi import OpenBazaarAPI
 from dht.storage import PersistentStorage
 
-TESTNET = True
+def run(*args):
+    TESTNET = args[0]
 
-# logging
-logFile = logfile.LogFile.fromFullPath(DATA_FOLDER + "debug.log")
-log.addObserver(log.FileLogObserver(logFile).emit)
-log.startLogging(sys.stdout)
+    # logging
+    logFile = logfile.LogFile.fromFullPath(DATA_FOLDER + "debug.log")
+    log.addObserver(log.FileLogObserver(logFile).emit)
+    log.startLogging(sys.stdout)
 
-# stun
-print "Finding NAT Type.."
-response = stun.get_ip_info(stun_host="stun.l.google.com", source_port=18467, stun_port=19302)
-print "%s on %s:%s" % (response[0], response[1], response[2])
-ip_address = response[1]
-port = response[2]
+    # stun
+    port = 18467 if not TESTNET else 28467
+    print "Finding NAT Type.."
+    response = stun.get_ip_info(stun_host="stun.l.google.com", source_port=port, stun_port=19302)
+    print "%s on %s:%s" % (response[0], response[1], response[2])
+    ip_address = response[1]
+    port = response[2]
 
-# database
-db = Database(TESTNET)
+    # database
+    db = Database(TESTNET)
 
-# key generation
-keys = KeyChain(db)
+    # key generation
+    keys = KeyChain(db)
 
-def on_bootstrap_complete(resp):
-    mlistener = MessageListenerImpl(ws_factory, db)
-    mserver.get_messages(mlistener)
-    mserver.protocol.add_listener(mlistener)
-    nlistener = NotificationListenerImpl(ws_factory, db)
-    mserver.protocol.add_listener(nlistener)
+    def on_bootstrap_complete(resp):
+        mlistener = MessageListenerImpl(ws_factory, db)
+        mserver.get_messages(mlistener)
+        mserver.protocol.add_listener(mlistener)
+        nlistener = NotificationListenerImpl(ws_factory, db)
+        mserver.protocol.add_listener(nlistener)
 
-protocol = OpenBazaarProtocol((ip_address, port), testnet=TESTNET)
+    protocol = OpenBazaarProtocol((ip_address, port), testnet=TESTNET)
 
-# kademlia
-node = Node(keys.guid, ip_address, port, signed_pubkey=keys.guid_signed_pubkey)
+    # kademlia
+    node = Node(keys.guid, ip_address, port, signed_pubkey=keys.guid_signed_pubkey)
 
-if os.path.isfile(DATA_FOLDER + 'cache.pickle'):
-    kserver = Server.loadState(DATA_FOLDER + 'cache.pickle', ip_address, port, protocol, db,
-                               on_bootstrap_complete, storage=PersistentStorage(db.DATABASE))
-else:
-    kserver = Server(node, db, dht.constants.KSIZE, dht.constants.ALPHA, storage=PersistentStorage(db.DATABASE))
-    kserver.protocol.connect_multiplexer(protocol)
-    kserver.bootstrap(
-        kserver.querySeed("162.213.253.147:8080",
-                          "5b56c8daeb3b37c8a9b47be6102fa43b9f069f58dcb57475984041b26c99e389"))\
-        .addCallback(on_bootstrap_complete)
+    if os.path.isfile(DATA_FOLDER + 'cache.pickle'):
+        kserver = Server.loadState(DATA_FOLDER + 'cache.pickle', ip_address, port, protocol, db,
+                                   on_bootstrap_complete, storage=PersistentStorage(db.DATABASE))
+    else:
+        kserver = Server(node, db, dht.constants.KSIZE, dht.constants.ALPHA, storage=PersistentStorage(db.DATABASE))
+        kserver.protocol.connect_multiplexer(protocol)
+        kserver.bootstrap(
+            kserver.querySeed("162.213.253.147:8080",
+                              "5b56c8daeb3b37c8a9b47be6102fa43b9f069f58dcb57475984041b26c99e389"))\
+            .addCallback(on_bootstrap_complete)
 
-kserver.saveStateRegularly(DATA_FOLDER + 'cache.pickle', 10)
-protocol.register_processor(kserver.protocol)
+    kserver.saveStateRegularly(DATA_FOLDER + 'cache.pickle', 10)
+    protocol.register_processor(kserver.protocol)
 
-# market
-mserver = network.Server(kserver, keys.signing_key, db)
-mserver.protocol.connect_multiplexer(protocol)
-protocol.register_processor(mserver.protocol)
+    # market
+    mserver = network.Server(kserver, keys.signing_key, db)
+    mserver.protocol.connect_multiplexer(protocol)
+    protocol.register_processor(mserver.protocol)
 
-reactor.listenUDP(port, protocol)
+    reactor.listenUDP(port, protocol)
 
-# websockets api
-ws_factory = WSFactory("ws://127.0.0.1:18466", mserver, kserver)
-ws_factory.protocol = WSProtocol
-ws_factory.setProtocolOptions(allowHixie76=True)
-listenWS(ws_factory)
-webdir = File(".")
-web = Site(webdir)
-reactor.listenTCP(9000, web, interface="127.0.0.1")
+    # websockets api
+    ws_factory = WSFactory("ws://127.0.0.1:18466", mserver, kserver)
+    ws_factory.protocol = WSProtocol
+    ws_factory.setProtocolOptions(allowHixie76=True)
+    listenWS(ws_factory)
+    webdir = File(".")
+    web = Site(webdir)
+    reactor.listenTCP(9000, web, interface="127.0.0.1")
 
-# blockchain
-if TESTNET:
-    libbitcoin_client = obelisk.ObeliskOfLightClient("tcp://testnet-baltic.airbitz.co:9091")
-else:
-    libbitcoin_client = obelisk.ObeliskOfLightClient("tcp://libbitcoin1.openbazaar.org:9091")
+    # rest api
+    api = OpenBazaarAPI(mserver, kserver, protocol)
+    site = Site(api, timeout=None)
+    reactor.listenTCP(18469, site, interface="127.0.0.1")
 
-# rest api
-api = OpenBazaarAPI(mserver, kserver, protocol)
-site = Site(api, timeout=None)
-reactor.listenTCP(18469, site, interface="127.0.0.1")
+    # blockchain
+    if TESTNET:
+        libbitcoin_client = obelisk.ObeliskOfLightClient("tcp://testnet-baltic.airbitz.co:9091")
+    else:
+        libbitcoin_client = obelisk.ObeliskOfLightClient("tcp://libbitcoin1.openbazaar.org:9091")
 
-protocol.set_servers(ws_factory, libbitcoin_client)
+    protocol.set_servers(ws_factory, libbitcoin_client)
 
-reactor.run()
+    reactor.run()
+
+if __name__ == "__main__":
+    # pylint: disable=anomalous-backslash-in-string
+    class OpenBazaard(Daemon):
+        def run(self, *args):
+            run(*args)
+
+    class Parser(object):
+        def __init__(self, daemon):
+            self.daemon = daemon
+            parser = argparse.ArgumentParser(
+                description='OpenBazaard v0.1',
+                usage='''
+    python openbazaard.py <command> [<args>]
+    python openbazaard.py <command> --help
+
+commands:
+    start            start the OpenBazaar server
+    stop             shutdown the server and disconnect
+    restart          restart the server
+''')
+            parser.add_argument('command', help='Execute the given command')
+            args = parser.parse_args(sys.argv[1:2])
+            if not hasattr(self, args.command):
+                parser.print_help()
+                exit(1)
+            getattr(self, args.command)()
+
+        def start(self):
+            parser = argparse.ArgumentParser(
+                description="Start the OpenBazaar server",
+                usage='''usage:
+        python openbazaard.py start [-d DAEMON]''')
+            parser.add_argument('-d', '--daemon', action='store_true', help="run the server in the background")
+            parser.add_argument('-t', '--testnet', action='store_true', help="use the test network")
+            args = parser.parse_args(sys.argv[2:])
+            OKBLUE = '\033[94m'
+            ENDC = '\033[0m'
+            print "________             " + OKBLUE + "         __________" + ENDC
+            print "\_____  \ ______   ____   ____" + OKBLUE + \
+                  "\______   \_____  _____________  _____ _______" + ENDC
+            print " /   |   \\\____ \_/ __ \ /    \\" + OKBLUE +\
+                  "|    |  _/\__  \ \___   /\__  \ \__  \\\_  __ \ " + ENDC
+            print "/    |    \  |_> >  ___/|   |  \    " + OKBLUE \
+                  + "|   \ / __ \_/    /  / __ \_/ __ \|  | \/" + ENDC
+            print "\_______  /   __/ \___  >___|  /" + OKBLUE + "______  /(____  /_____ \(____  (____  /__|" + ENDC
+            print "        \/|__|        \/     \/  " + OKBLUE + "     \/      \/      \/     \/     \/" + ENDC
+            print
+            print "OpenBazaar Server v0.1 starting..."
+            unix = ("linux", "linux2", "darwin")
+            if args.daemon and platform.system().lower() in unix:
+                self.daemon.start(args.testnet)
+            else:
+                run(args.testnet)
+
+        def stop(self):
+            # pylint: disable=W0612
+            parser = argparse.ArgumentParser(
+                description="Shutdown the server and disconnect",
+                usage='''usage:
+        python openbazaard.py stop''')
+            print "OpenBazaar server stopping..."
+            self.daemon.stop()
+
+        def restart(self):
+            # pylint: disable=W0612
+            parser = argparse.ArgumentParser(
+                description="Restart the server",
+                usage='''usage:
+        python openbazaard.py restart''')
+            print "Restarting OpenBazaar server..."
+            self.daemon.restart()
+
+    Parser(OpenBazaard('/tmp/daemon-example.pid'))
