ae445dc27bd2ebb357ce88e60fb78d2e7434eafa
foxcarlos <foxcarlos@gmail.com>
2015-10-17 20:02:21 -0430

    now the port is configured separatedly in the file config

--- constants.py
+++ constants.py
@@ -6,7 +6,6 @@ dataFolderPath = expanduser('~')
 currentPath = getcwdu()
 myFileConfig = 'constants.cfg'
 finalFileConfig = join(currentPath, myFileConfig)
-
 fc = ConfigParser.ConfigParser()
 fc.read(finalFileConfig)
 
@@ -16,14 +15,21 @@ if fc.has_section(mySection):
     items = fc.items(mySection)
     values = [value[1] for value in items]
 
-    data_f, seed_n, seed_n_t, ks, alp, trans_f = values
+    data_f, seed_n, seed_n_p, seed_n_t, seed_n_t_p, ks, alp, trans_f = values
 
     DATA_FOLDER = data_f
-    SEED_NODE = seed_n
-    SEED_NODE_TESTNET = seed_n_t
+    SEED_NODE = (seed_n, seed_n_p)
+    SEED_NODE_TESTNET = (seed_n_t, seed_n_t_p)
     KSIZE = ks
     ALPHA = alp
     TRANSACTION_FEE = trans_f
+    print DATA_FOLDER
+    print SEED_NODE
+    print SEED_NODE_TESTNET
+    print KSIZE
+    print ALPHA
+    print TRANSACTION_FEE
+
 
 '''
 DATA_FOLDER = expanduser("~") + "/OpenBazaar/"
