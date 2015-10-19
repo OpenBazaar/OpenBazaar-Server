9383a47b8a68ea74f93ff5d0412aed82d170466c
FoxCarlos Trabajo <foxcarlos@gmail.com>
2015-10-19 15:45:53 -0430

    now allow empty options inside file config for testing

--- constants.py
+++ constants.py
@@ -6,16 +6,23 @@ dataFolderPath = expanduser('~')
 currentPath = getcwdu()
 myFileConfig = 'ob.cfg'
 finalFileConfig = join(currentPath, myFileConfig)
-fc = ConfigParser.ConfigParser()
+fc = ConfigParser.ConfigParser(allow_no_value=True)
 fc.read(finalFileConfig)
 
+#name of section in Config File ob.cfg
 mySection = 'CONSTANTS'
 
+#if exist the sections in file config
 if fc.has_section(mySection):
-    items = fc.items(mySection)
-    values = [value[1] for value in items]
-
-    data_f, seed_n, seed_n_p, seed_n_t, seed_n_t_p, ks, alp, trans_f = values
+    #capture info
+    data_f = join(currentPath, fc.get(mySection, 'DATA_FOLDER'))
+    seed_n = fc.get(mySection, 'SEED_NODE')
+    seed_n_p = fc.getint(mySection, 'SEED_NODE_PORT')
+    seed_n_t = fc.get(mySection, 'SEED_NODE_TESTNET')
+    seed_n_t_p = fc.getint(mySection, 'SEED_NODE_TESTNET_PORT')
+    ks = fc.getint(mySection, 'KSIZE')
+    alp = fc.getint(mySection, 'ALPHA' )
+    trans_f  = fc.getint(mySection, 'TRANSACTION_FEE')
 
     DATA_FOLDER = data_f
     SEED_NODE = (seed_n, seed_n_p)
@@ -24,23 +31,8 @@ if fc.has_section(mySection):
     ALPHA = alp
     TRANSACTION_FEE = trans_f
 
-    print DATA_FOLDER
-    print SEED_NODE
-    print SEED_NODE_TESTNET
-    print KSIZE
-    print ALPHA
-    print TRANSACTION_FEE
-
-
 '''
-DATA_FOLDER = expanduser("~") + "/OpenBazaar/"
-SEED_NODE = ("205.186.154.163", 18467)
-SEED_NODE_TESTNET = ("205.186.154.163", 28467)
-KSIZE = 20
-ALPHA = 3
-TRANSACTION_FEE = 10000'''
-
-'''from os.path import expanduser
+from os.path import expanduser
 
 # TODO: this can be loaded from a config file
 
