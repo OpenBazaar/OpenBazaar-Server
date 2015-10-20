2cd8c9e257dc0bd3aad014e227e8d031e61713f1
foxcarlos <foxcarlos@gmail.com>
2015-10-19 22:16:57 -0430

    fixed bug when capture value DATA_FOLDER

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
-from os import getcwdu
+from os import getcwd
 from os.path import expanduser, join
 import ConfigParser
 
 dataFolderPath = expanduser('~')
-currentPath = getcwdu()
+currentPath = getcwd()
 myFileConfig = 'ob.cfg'
 finalFileConfig = join(currentPath, myFileConfig)
 fc = ConfigParser.ConfigParser(allow_no_value=True)
@@ -15,7 +15,8 @@ mySection = 'CONSTANTS'
 #if exist the sections in file config
 if fc.has_section(mySection):
     #capture info
-    data_f = join(currentPath, fc.get(mySection, 'DATA_FOLDER'))
+    data_f1 = join(dataFolderPath, fc.get(mySection, 'DATA_FOLDER'))
+    data_f2 = join(data_f1, '')  # is necesary for "/" the end
     seed_n = fc.get(mySection, 'SEED_NODE')
     seed_n_p = fc.getint(mySection, 'SEED_NODE_PORT')
     seed_n_t = fc.get(mySection, 'SEED_NODE_TESTNET')
@@ -24,7 +25,7 @@ if fc.has_section(mySection):
     alp = fc.getint(mySection, 'ALPHA' )
     trans_f  = fc.getint(mySection, 'TRANSACTION_FEE')
 
-    DATA_FOLDER = data_f
+    DATA_FOLDER = data_f2
     SEED_NODE = (seed_n, seed_n_p)
     SEED_NODE_TESTNET = (seed_n_t, seed_n_t_p)
     KSIZE = ks
