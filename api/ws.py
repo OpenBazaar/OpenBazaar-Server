0772d1f8207c52ce61f6f4d562ee9ea2be8bf686
Chris Pacia <ctpacia@gmail.com>
2015-10-23 11:47:54 -0400

    Fix get_homepage_listings looping query

--- api/ws.py
+++ api/ws.py
@@ -159,6 +159,7 @@ class WSProtocol(WebSocketServerProtocol):
                         count += 1
                         self.factory.outstanding[message_id].append(l.contract_hash)
                         if count == 3:
+                            vendors.remove(node)
                             return count
                 vendors.remove(node)
             else:
