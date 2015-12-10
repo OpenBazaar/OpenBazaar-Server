1a61fc8b57bce57d3ef77ca6941e213b998fca0b
Chris Pacia <ctpacia@gmail.com>
2015-12-10 12:27:55 -0500

    Bump protocol version

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ from os import getcwd
 from os.path import expanduser, join, isfile
 import ConfigParser
 
-PROTOCOL_VERSION = 6
+PROTOCOL_VERSION = 7
 
 dataFolderPath = expanduser('~')
 currentPath = getcwd()
