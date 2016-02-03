b849219c476db73e11ba40900d5a558a7b3e7759
Chris Pacia <ctpacia@gmail.com>
2016-02-03 17:06:32 -0500

    Add pause during looping retry

--- net/utils.py
+++ net/utils.py
@@ -1,6 +1,10 @@
+import time
+
+
 def looping_retry(func, *args):
     while True:
         try:
             return func(*args)
         except Exception:
+            time.sleep(0.5)
             pass
