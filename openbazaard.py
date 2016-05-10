a4902caf8a2319125d2cfac6f1e51b8c2e961a75
Chris Pacia <ctpacia@gmail.com>
2016-05-10 12:44:19 -0400

    Bump version

--- openbazaard.py
+++ openbazaard.py
@@ -214,7 +214,7 @@ if __name__ == "__main__":
         def __init__(self, daemon):
             self.daemon = daemon
             parser = argparse.ArgumentParser(
-                description='OpenBazaar-Server v0.1.8',
+                description='OpenBazaar-Server v0.2.0',
                 usage='''
     python openbazaard.py <command> [<args>]
     python openbazaard.py <command> --help
