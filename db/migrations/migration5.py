bc9656c1ced31f0592b6d73a0678386843afa5b5
Chris Pacia <ctpacia@gmail.com>
2016-05-18 15:44:33 -0400

    Initialize unread column to 0

--- db/migrations/migration5.py
+++ db/migrations/migration5.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def migrate(database_path):
     print "migrating to db version 5"
     conn = sqlite3.connect(database_path)
@@ -10,6 +11,10 @@ def migrate(database_path):
     cursor.execute('''ALTER TABLE sales ADD COLUMN "unread" INTEGER''')
     cursor.execute('''ALTER TABLE purchases ADD COLUMN "unread" INTEGER''')
     cursor.execute('''ALTER TABLE cases ADD COLUMN "unread" INTEGER''')
+
+    cursor.execute('''UPDATE purchases SET unread = 0;''')
+    cursor.execute('''UPDATE purchases SET unread = 0;''')
+    cursor.execute('''UPDATE purchases SET unread = 0;''')
 
     # update version
     cursor.execute('''PRAGMA user_version = 5''')
