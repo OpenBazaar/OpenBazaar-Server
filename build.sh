d71370512cb53900038c0db790f8c3f3749afe1c
Brian Hoffman <brianchoffman@gmail.com>
2016-09-08 15:38:12 -0400

    Updates

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ case "$TRAVIS_OS_NAME" in
 
     echo "Building Linux Binaries...."
 
+    echo "32-bit..."
+
     mkdir dist/linux32
     cd dist/linux32
 
@@ -23,7 +25,25 @@ case "$TRAVIS_OS_NAME" in
     pip install -r ../../requirements.txt
     pip install pyinstaller==3.1
     pip install cryptography
-    pyinstaller -D -F -n openbazaard-linux32 ../../openbazaard.py
+    pyinstaller -D -F ../../.travis/openbazaard.linux32.spec
+
+    echo "64-bit"
+
+    mkdir dist/linux64
+    cd dist/linux64
+
+    echo "Installing APT packages"
+    sudo apt-get install npm python-pip python-virtualenv python-dev libffi-dev
+
+    echo "Set up virtualenv"
+    virtualenv env
+    . env/bin/activate
+
+    echo "Install Python dependencies"
+    pip install -r ../../requirements.txt
+    pip install pyinstaller==3.1
+    pip install cryptography
+    pyinstaller -D -F ../../.travis/openbazaard.linux64.spec
 
     ;;
 
