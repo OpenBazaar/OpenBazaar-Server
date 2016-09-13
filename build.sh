1057a2974cf6e323d103804f9a84b7e04c0b0c6a
Brian Hoffman <brianchoffman@gmail.com>
2016-09-13 13:50:13 -0400

    Updates

--- build.sh
+++ build.sh
@@ -11,6 +11,13 @@ case "$TRAVIS_OS_NAME" in
 
     echo "32-bit..."
 
+    wget https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
+    tar xzvf Python-2.7.11.tgz
+    sudo apt-get install gcc-multilib g++-multilib
+    CFLAGS=-m32 LDFLAGS=-m32 ./configure --prefix=/opt/Python2.7-32bits
+    make
+    sudo make install
+
     mkdir dist/linux32
     cd dist/linux32
 
@@ -25,27 +32,21 @@ case "$TRAVIS_OS_NAME" in
     pip install -r ../../requirements.txt
     pip install pyinstaller==3.1
     pip install cryptography
-    pyinstaller -D -F ../../.travis/openbazaard.linux32.spec
+    pip install setuptools==19.2
+    cd ../..
+    pyinstaller -D -F ./.travis/openbazaard.linux32.spec
 
-    cd ../../
-
-    echo "64-bit"
-
-    mkdir dist/linux64
-    cd dist/linux64
-
-    echo "Installing APT packages"
-    sudo apt-get install npm python-pip python-virtualenv python-dev libffi-dev
-
-    echo "Set up virtualenv"
-    virtualenv env
-    . env/bin/activate
-
-    echo "Install Python dependencies"
-    pip install -r ../../requirements.txt
-    pip install pyinstaller==3.1
-    pip install cryptography
-    pyinstaller -D -F ../../.travis/openbazaard.linux64.spec
+    # echo "64-bit"
+    #
+    # mkdir dist/linux64
+    # cd dist/linux64
+    #
+    # echo "Set up virtualenv"
+    # virtualenv env
+    # . env/bin/activate
+    #
+    # echo "Install Python dependencies"
+    # pyinstaller -D -F ../../.travis/openbazaard.linux64.spec
 
     ;;
 
