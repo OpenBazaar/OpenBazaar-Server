6aa2450fd31e37549f175524f6a691052952d749
Brian Hoffman <brianchoffman@gmail.com>
2016-10-04 14:43:06 -0400

    Move to curl

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ mkdir dist
     echo "Building Linux Binaries...."
 
     echo "32-bit..."
-
-    wget https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
+    
+    curl https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz -o Python-2.7.11.tgz
     tar xzvf Python-2.7.11.tgz
     sudo apt-get -y install gcc-multilib g++-multilib
     CFLAGS=-m32 LDFLAGS=-m32 ./configure --prefix=/opt/Python2.7-32bits
