bd24bcc12fefff88830359b4a8f4aa649e187d55
Brian Hoffman <brianchoffman@gmail.com>
2016-10-05 15:21:17 -0400

    Remove quiet

--- build-win64.sh
+++ build-win64.sh
@@ -66,7 +66,7 @@ wine cmd /c "copy c:\\Windows\\System32\\msvcm90.dll c:\\python27"
 wine cmd /c "copy c:\\Windows\\System32\\msvcm90.dll c:\\python27"
 
 wget -N https://github.com/pyinstaller/pyinstaller/releases/download/v3.1/PyInstaller-3.1.zip
-unzip -q -o PyInstaller-3.1.zip
+unzip -o PyInstaller-3.1.zip
 cd ../..
 wine C:/Python27/python.exe dist/windows64/PyInstaller-3.1/pyinstaller.py -F -n openbazaard.exe -i images/icon.ico .travis/openbazaard.win64.spec --noconfirm
 wine cmd 
